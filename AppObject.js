7fa5eadc41f64f38f43a69c5f18e34fabb4a4a2b
8Macosta <8racosta@stpatrick.org>
2016-10-12 11:27:09 -0500

    Made Transformers game with Health, Mana, & Strength

---
+++ AppObject.js
@@ -0,0 +1,34 @@
+function Transformers(T){
+
+    //Declare your instance variables here.
+    this.Health
+    this.Mana
+    this.Strength
+
+    //Declare the instance function open here.
+    this.open = functionTransformers(T){
+
+
+
+    };
+    //Declare the instance function sleep here.
+
+
+
+    //Declare the instance function active here.
+
+
+
+    //Declare the instance function close here.
+
+
+
+
+}
+
+//Use the constructor to create 4 App objects below.
+
+
+
+//Declare an array named appList, and place all 4 of your apps in that array.
+//The order does not matter.
